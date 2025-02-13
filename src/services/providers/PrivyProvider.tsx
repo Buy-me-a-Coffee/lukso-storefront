39b761e66ae6659e90536a8f9a2c530298b716e2
rene.vucko <rene.vucko@comtradegaming.com>
2025-02-13 14:13:03 +0100

    fix

--- src/services/providers/PrivyProvider.tsx
+++ src/services/providers/PrivyProvider.tsx
@@ -1,25 +1,6 @@
 import {PrivyProvider} from '@privy-io/react-auth';
 import logo from "../../assets/react.svg";
 
-const luksoChain = {
-  id: 42,
-  name: 'LUKSO',
-  network: 'lukso',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'LYX',
-    symbol: 'LYX',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.lukso.network'],
-    },
-  },
-  blockExplorers: {
-    default: { name: 'LUKSO Explorer', url: 'https://explorer.lukso.network' },
-  },
-};
-
 export default function Privy({children}: {children: React.ReactNode}) {
   return (
     <PrivyProvider
@@ -31,12 +12,9 @@ export default function Privy({children}: {children: React.ReactNode}) {
           accentColor: '#de2f74',
           logo: logo,
         },
-        // Create embedded wallets for users who don't have a wallet
-        embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
+        loginMethodsAndOrder: {
+          primary: ['universal_profile']
         },
-        supportedChains: [luksoChain],
-        defaultChain: luksoChain,
       }}
     >
       {children}
